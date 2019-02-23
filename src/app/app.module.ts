837a27d7ab24cd7455edf05952f0705f585ed581
Cort Harlow <cort.harlow@gmail.com>
2019-02-23 16:01:21 -0500

    Begin application structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { LandingPageComponent } from './landing/landing-page.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent
   ],
   imports: [
     BrowserModule,
